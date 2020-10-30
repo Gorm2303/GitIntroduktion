df49e0e5f20c9a358c2ca171cbe4b434af75a9c8
Gorm <gokri20@student.sdu.dk>
2020-10-30 13:19:56 +0100

    not main
    not main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("this is dudu");
         System.out.println("this is dudu");
         System.out.println("this is dudu");
+        System.out.println("This is part 2, not main");
     }
 }
